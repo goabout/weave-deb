9880dd83c135a5a51a77e0b04715e31156b9a7c5
Joost Cassee <joost@cassee.net>
2016-05-31 00:16:33 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+NAME=weave
+BINARY=/usr/sbin/weave
+VERSION=1.5.2
+DESCRIPTION='Simple, resilient multi-host Docker networking and more'
+URL=https://github.com/weaveworks/weave
+VENDOR='GoAbout'
+MAINTAINER='Joost Cassee <joost.cassee@goabout.com>'
+LICENSE='Apache License 2.0'
+
+#wget -O dist$BINARY https://github.com/weaveworks/weave/releases/download/v$VERSION/weave
+#chmod 0755 dist$BINARY
+
+rm -f weave_${VERSION}_amd64.deb
+fpm -t deb -s dir -n $NAME -v $VERSION --description "$DESCRIPTION" -C dist \
+  --vendor "$VENDOR" -m "$MAINTAINER" --license "$LICENSE" --url $URL \
+  --after-install scripts/after-install --before-remove scripts/before-remove \
+  --deb-changelog changelog --deb-no-default-config-files --config-files etc --log debug .
